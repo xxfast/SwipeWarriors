bc61d0455a598f96697a4a91233980d101632bce
xxfast <isuru@xfastgames.com>
2017-04-10 17:16:58 +1000

    updated some of the assets
    now they have rigid bodies

---
+++ Assets/Collapsible.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collapsible : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(collision.gameObject.tag.Contains("enemy")){
+			return;
+		}
+	}
+}
