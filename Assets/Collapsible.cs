10c68e9798279e86cdc866b9da3f610544b69972
xxfast <isuru@xfastgames.com>
2017-04-10 17:30:45 +1000

    commented out broken collapsable nature
    TODO

--- Assets/Collapsible.cs
+++ Assets/Collapsible.cs
@@ -14,8 +14,15 @@ public class Collapsible : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
+		// TODO : Implement collapsable behavior
 		if(collision.gameObject.tag.Contains("enemy")){
-			return;
+//			Object prefab = Resources.Load("enemy", typeof(GameObject));
+//			GameObject enemy = Instantiate(prefab, this.transform.position, new Quaternion(0,0,0,0)) as GameObject;
+//			Vector3 newScale = Vector3.Scale (this.transform.localScale, collision.gameObject.transform.localScale);
+//			enemy.transform.localScale.Set (newScale.x*2,newScale.y*2,newScale.z*2);
+//			enemy.GetComponent<FollowTarget> ().target = this.gameObject;
+//			Destroy (collision.gameObject);
+//			Destroy (this.gameObject);
 		}
 	}
 }
