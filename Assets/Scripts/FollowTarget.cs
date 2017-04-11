3f88f444c4085bd55b6c811f9a60860badc973c9
xxfast <isuru@xfastgames.com>
2017-04-11 19:27:49 +1000

    made use of rigid body physics and used force based motion, instead of static motion

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -5,35 +5,24 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour {
 
 	public GameObject target;
-	[Range(0f,0.1f)] public float followSpeed;
+	[Range(0f,100f)] public float followSpeed;
 	[Range(1f,10f)] public float rotationSpeed;
 
 	void Start () {
-	}
-
-	void Update () {
 		if(target){
-			this.rotate (rotationSpeed);
-			var d = distanceTo(target.transform.position);
+			var d = Vector2.Distance (this.transform.position,target.transform.position);
 			if (d > target.transform.localScale.x/2){
 				var dx = (target.transform.position.x - this.transform.position.x) / d;
 				var dy = (target.transform.position.y - this.transform.position.y) / d;
-				this.move(dx * followSpeed, dy * followSpeed );
+				this.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2(dx * followSpeed, dy * followSpeed));
 			}
+			this.gameObject.transform.Rotate (new Vector3 (0, 0, 0),rotationSpeed);
+			this.gameObject.GetComponent<Rigidbody2D> ().AddTorque (rotationSpeed);
 		}
 	}
 
-	private float distanceTo(Vector2 point){
-		Vector2 a = this.transform.position;
-		Vector2 b = point;
-		return Mathf.Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+	void Update () {
+		
 	}
 
-	private void move(float x, float y){
-		this.transform.position = new Vector2 (this.transform.position.x + x, this.transform.position.y + y);
-	}
-
-	private void rotate(float by){
-		this.transform.Rotate (new Vector3 (0, 0, by));
-	}
 }
