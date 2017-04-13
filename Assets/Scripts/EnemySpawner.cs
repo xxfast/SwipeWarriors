9815e33c985c4dec29ab04580c10ca8ec0c7bb88
xxfast <isuru@xfastgames.com>
2017-04-13 17:26:08 +1000

    fixed the enemy spawner script to spawn all kind of enemies
    this is just for the demonstration of newly added enemy types

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -6,8 +6,9 @@ public class EnemySpawner : MonoBehaviour {
 
 	public float spawnDistance = 10;
 	public float spanwFrequency = 3.0f;
-
+	public List<Object> whatToSpawn = new List<Object> ();
 	private float time;
+
 
 	void Start () {
 		time = spanwFrequency;
@@ -26,9 +27,12 @@ public class EnemySpawner : MonoBehaviour {
 		var radius = spawnDistance;
 		var x = Mathf.Cos (angle) * radius + this.transform.position.x;
 		var y = Mathf.Sin (angle) * radius +  this.transform.position.y;
-		//var myIntruder = new Intruder (scene).targets (myKingdom).killer (myWarrior).translate (x, y);
-		Object prefab = Resources.Load("enemy", typeof(GameObject));
-		GameObject enemy = Instantiate(prefab, new Vector3(x,y,0), new Quaternion(0,0,0,0)) as GameObject;
-		enemy.GetComponent<FollowTarget> ().target = this.gameObject;
+		int index = Random.Range (0, whatToSpawn.Count - 1);
+		Debug.Log ("Range = "+ index);
+		Object prefab = whatToSpawn[index];
+		if (prefab) {
+			GameObject enemy = Instantiate (prefab, new Vector3 (x, y, 0), new Quaternion (0, 0, 0, 0)) as GameObject;
+			enemy.GetComponent<FollowTarget> ().target = this.gameObject;
+		}
 	}
 }
