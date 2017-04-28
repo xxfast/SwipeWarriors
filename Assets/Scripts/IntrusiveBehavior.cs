e9e330bf036de7eb1663f6afe0fad464ab18798b
xxfast <isuru@xfastgames.com>
2017-04-28 18:51:01 +1000

    added the Intrusive behaviour and the reproductive behaviour
    also changed the enemy health behaviour because it wasn’t killing it

---
+++ Assets/Scripts/IntrusiveBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntrusiveBehavior : MonoBehaviour {
+
+	public GameObject toIntrude;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D theCollider)
+	{
+		if (theCollider.gameObject.CompareTag("obstacle"))
+		{
+			this.gameObject.transform.SetParent(theCollider.gameObject.transform);
+			this.gameObject.GetComponent<FollowTarget>().enabled = false;
+			Destroy (this.gameObject.GetComponent<Rigidbody2D> ());
+		}
+	}
+
+}
---
+++ Assets/Scripts/ReproductiveBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReproductiveBehavior : MonoBehaviour {
+
+	public List<GameObject> toReproduce; 
+	public int frequency;
+	public GameObject targetOfTheReproduced;
+
+	private float time;
+
+	void Start () {
+		time = frequency;
+	}
+
+	void Update () {
+		time -= Time.deltaTime;
+		if (time<=0) {
+			Reproduce ();
+			time = frequency;
+		}
+	}
+
+	void Reproduce(){
+		if (toReproduce != null && toReproduce.Count>0) {
+			int index = Random.Range (0, toReproduce.Count - 1);
+			Object prefab = toReproduce[index];
+			if (prefab) {
+				GameObject enemy = Instantiate (prefab, this.gameObject.transform.position, this.gameObject.transform.localRotation) as GameObject;
+				enemy.GetComponent<FollowTarget> ().target = targetOfTheReproduced;
+			}
+		}
+	}
+
+
+}
