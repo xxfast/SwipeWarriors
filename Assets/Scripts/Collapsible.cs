b37434f0dcaa828c219f6ab7c9d767bae112f5b8
xxfast <isuru@xfastgames.com>
2017-04-29 23:23:05 +1000

    updated the prefabs with correct sizes
    the images were too big

--- Assets/Scripts/Collapsible.cs
+++ Assets/Scripts/Collapsible.cs
@@ -14,15 +14,12 @@ public class Collapsible : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		// TODO : Implement collapsable behavior
 		if(collision.gameObject.tag.Contains("enemy")){
-//			Object prefab = Resources.Load("enemy", typeof(GameObject));
-//			GameObject enemy = Instantiate(prefab, this.transform.position, new Quaternion(0,0,0,0)) as GameObject;
-//			Vector3 newScale = Vector3.Scale (this.transform.localScale, collision.gameObject.transform.localScale);
-//			enemy.transform.localScale.Set (newScale.x*2,newScale.y*2,newScale.z*2);
-//			enemy.GetComponent<FollowTarget> ().target = this.gameObject;
-//			Destroy (collision.gameObject);
-//			Destroy (this.gameObject);
+			collision.gameObject.GetComponent<Collapsible>().enabled = false;
+			this.gameObject.transform.SetParent(collision.gameObject.transform);
+			this.gameObject.GetComponent<FollowTarget>().enabled = false;
+			this.gameObject.GetComponent<PolygonCollider2D>().enabled = true;
+			Destroy (this.gameObject.GetComponent<Rigidbody2D> ());
 		}
 	}
 }
