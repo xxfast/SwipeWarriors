b1dc404a0f0f76f2fe7230d2028d21a59a2c89e8
xxfast <isuru@xfastgames.com>
2017-04-29 23:37:11 +1000

    updated colliders
    the colliders were mismatching

--- Assets/Scripts/Collapsible.cs
+++ Assets/Scripts/Collapsible.cs
@@ -15,11 +15,7 @@ public class Collapsible : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		if(collision.gameObject.tag.Contains("enemy")){
-			collision.gameObject.GetComponent<Collapsible>().enabled = false;
-			this.gameObject.transform.SetParent(collision.gameObject.transform);
-			this.gameObject.GetComponent<FollowTarget>().enabled = false;
-			this.gameObject.GetComponent<PolygonCollider2D>().enabled = true;
-			Destroy (this.gameObject.GetComponent<Rigidbody2D> ());
+			collision.gameObject.transform.SetParent(this.gameObject.transform);
 		}
 	}
 }
