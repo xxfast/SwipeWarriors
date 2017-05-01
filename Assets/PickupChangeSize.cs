4da63d9bf2a8a4ae4250891f1232f544033cb64d
James Galovic <jpgalovic@gmail.com>
2017-05-02 01:29:42 +1000

    Add: Size Change Functionality and Reversion timer
    Togglable for stackable effect (stackable adds delta size, non stakable
    sets size) no matter if stakable collection of another item will reset
    timer.

---
+++ Assets/PickupChangeSize.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupChangeSize : MonoBehaviour {
+
+    public float SetSize;
+    public float DeltaSize;
+
+    public int Duration;
+
+    public bool Stakable;
+    public bool DebugMessage;
+
+    // executes on collect. (called by player)
+    public void onCollect(GameObject gameObject)
+    {
+        if (DebugMessage)
+            Debug.Log("Changing Size");
+
+        if(Stakable)
+        {
+            Vector3 scale = new Vector3(SetSize, SetSize);
+            gameObject.transform.localScale = scale;
+        }
+        else
+        {
+            Vector3 scale = new Vector3(DeltaSize, DeltaSize);
+            gameObject.transform.localScale += scale;
+        }
+
+        gameObject.SendMessage("SetSizeRevertTimer", Duration);
+
+        Destroy(this.gameObject);
+    }
+}
--- Assets/Scripts/PickupStaminaBoost.cs
+++ Assets/Scripts/PickupStaminaBoost.cs
@@ -3,25 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupStaminaBoost : MonoBehaviour {
-    //refernces
-    public StaminaBar staminaBar;
-
     //variables
     public int recoveryAmount;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     // executes on collect. (called by player)
-    public void onCollect()
+    public void onCollect(GameObject gameObject)
     {
-        staminaBar.applyDelta(recoveryAmount);
+        gameObject.GetComponent<StaminaBar>().applyDelta(recoveryAmount);
     }
 }
