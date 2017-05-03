69c2497f11a03449926ea8954de0e72c445892f1
xxfast <isuru@xfastgames.com>
2017-05-03 21:30:47 +1000

    added the shooting behaviour and the bullet prefab
    shooting behaviour can be customised for range and frequency, while the
    bullet can be customised for speed and damage

---
+++ Assets/Scripts/DespawnBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DespawnBehavior : MonoBehaviour {
+
+	public float lifeTime = 10.0f;
+
+	private float time;
+
+	void Start () {
+		time = lifeTime;
+	}
+	
+	void Update () {
+		time -= Time.deltaTime;
+		if(time<=0){
+			Destroy (this.gameObject);
+		}
+	}
+}
