9b0bc5ed1e17ae1f09d264e9b37f41c4ed123e48
Xavier-Gibbon <101104563@student.swin.edu.au>
2017-05-03 20:44:27 +1000

    Modified: Player now takes time to rotate at full speed or to stop rotating

--- Assets/Scripts/SpinWhileMoving.cs
+++ Assets/Scripts/SpinWhileMoving.cs
@@ -5,13 +5,33 @@ using UnityEngine;
 public class SpinWhileMoving : MonoBehaviour {
 
     public StaminaBar staminaBar;
+    public float spinAccelerator;
+    public float rotationAmount;
+    private float spinModifier = 0;
 	
 	// Update is called once per frame
 	void Update () {
-        //While moving, the player will spin
+        //While moving, the player will spin faster, until it reaches its max rotation speed
 		if (staminaBar.moving)
         {
-            this.transform.Rotate(0, 0, 720 * Time.deltaTime);
+            //The spinModifer variable makes
+            spinModifier += spinAccelerator * Time.deltaTime;
+            if (spinModifier > 1)
+            {
+                spinModifier = 1;
+            }            
         }
-	}
+        //Otherwise the player will spin slower, until the player stops rotating
+        else
+        {
+            spinModifier -= spinAccelerator * Time.deltaTime;
+            if (spinModifier < 0)
+            {
+                spinModifier = 0;
+            }
+        }
+
+        //Then rotate it!
+        this.transform.Rotate(0, 0, rotationAmount * Time.deltaTime * spinModifier);
+    }
 }
