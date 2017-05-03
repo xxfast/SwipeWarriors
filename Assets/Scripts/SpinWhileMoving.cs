0d1d63fee61952ac21305b3a97d5770407d05e2b
Xavier-Gibbon <101104563@student.swin.edu.au>
2017-05-03 20:30:55 +1000

    Add: Sword attack area and player spinning while moving

---
+++ Assets/Scripts/SpinWhileMoving.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinWhileMoving : MonoBehaviour {
+
+    public StaminaBar staminaBar;
+	
+	// Update is called once per frame
+	void Update () {
+        //While moving, the player will spin
+		if (staminaBar.moving)
+        {
+            this.transform.Rotate(0, 0, 720 * Time.deltaTime);
+        }
+	}
+}
