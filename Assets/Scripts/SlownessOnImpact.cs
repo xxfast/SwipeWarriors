64d7fdc8163f49811222a6ee828c77ebf059bbc5
xxfast <isuru@xfastgames.com>
2017-05-06 14:49:40 +1000

    updated the scene

---
+++ Assets/Scripts/SlownessOnImpact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlownessOnImpact : MonoBehaviour {
+
+	public float slownessFactor = 10;
+	public float speed;
+	public GameObject obj;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (obj)
+			speed = obj.GetComponent<PathMovement> ().speed;
+	}
+
+	private void OnTriggerEnter2D(Collider2D e)
+	{
+		obj = e.gameObject;
+		e.gameObject.GetComponent<PathMovement> ().speed = gameObject.GetComponent<PathMovement>().speed / slownessFactor;
+	}
+
+	private void OnTriggerExit2D(Collider2D e)
+	{
+		obj = e.gameObject;
+		e.gameObject.GetComponent<PathMovement> ().speed = gameObject.GetComponent<PathMovement>().speed * slownessFactor;
+	}
+}
