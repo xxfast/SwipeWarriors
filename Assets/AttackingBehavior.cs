a8ac4944de693d2e3b0201aa96883a284ab2e38e
xxfast <isuru@xfastgames.com>
2017-05-07 18:34:41 +1000

    Added a attacking behaviour to the enemy
    rewriting the “PlayerAndEnemyHit” component preventing circular
    dependencies

---
+++ Assets/AttackingBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackingBehavior : MonoBehaviour {
+
+	public float attackFrequency = 1;
+	private int attackDamage = 1;
+	public string attackAnyoneFlagged;
+
+	private float time;
+	private float isAllowedToAttack = false;
+
+	void Start(){
+		time = attackFrequency;
+	}
+
+	void Update () {
+		time -= Time.deltaTime;
+		isAllowedToAttack = (time <= 0);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if(isAllowedToAttack){
+			if(collision.gameObject.CompareTag(attackAnyoneFlagged)){
+				Health health;
+				if(health = collision.gameObject.GetComponent<Health>()){
+					health.DealDamage (attackDamage);
+				}
+			}
+		}
+	}
+}
