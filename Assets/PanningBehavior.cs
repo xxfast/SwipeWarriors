a4f2b24770618870ab68b20939e4df195178c356
xxfast <isuru@xfastgames.com>
2017-05-07 18:12:10 +1000

    added panning behaviour
    looks like the path movement script is using relative camera
    coordinates, so that seems to be broken right now

---
+++ Assets/PanningBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanningBehavior : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject anchor;
+
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = player.transform.position - anchor.transform.position;
+		newPosition.z -= 100;
+		this.transform.position =  newPosition;
+	}
+}
