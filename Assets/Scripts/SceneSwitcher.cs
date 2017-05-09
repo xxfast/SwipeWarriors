77529b9bbda0c999f5ec72b72382c2b73369eead
xxfast <isuru@xfastgames.com>
2017-05-09 13:24:28 +1000

    updated scene

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -1,16 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	public void SwitchScene(string toload){
+		SceneManager.LoadScene(toload, LoadSceneMode.Additive);
 	}
 }
