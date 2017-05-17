e196cc0b135e2bb3f5da300527b9381f231b3c9c
xxfast <isuru@xfastgames.com>
2017-05-17 12:27:38 +1000

    added the Retreating behaviour and RiskAversionBehavior

---
+++ Assets/RetreatingBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FollowTarget))]
+[RequireComponent(typeof(RiskAversionBehavior))]
+public class RetreatingBehavior : MonoBehaviour {
+
+	public float noticeDistance;
+	public float retreatDistance;
+
+	private GameObject target;
+
+	void Start () {
+		target = gameObject.GetComponent<FollowTarget> ().target;
+	}
+	
+	void Update () {
+		float targetDistance = Vector2.Distance (gameObject.transform.position, target.transform.position);
+		if(targetDistance<=noticeDistance){
+			if(!gameObject.GetComponent<RiskAversionBehavior>().IsAbleToDefeatEnemy(target)){
+				FleeFromTarget ();
+			}
+		}
+	}
+
+	public void FleeFromTarget(){
+		gameObject.GetComponent<FollowTarget> ().target = null;
+	}
+}
---
+++ Assets/RiskAversionBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+[RequireComponent(typeof(AttackingBehavior))]
+public class RiskAversionBehavior : MonoBehaviour {
+	
+	public bool IsAbleToDefeatEnemy(GameObject enemy){
+		float selfHealth = this.gameObject.GetComponent<Health> ().CurrentHealth;
+		float enemyHealth = enemy.GetComponent<Health> ().CurrentHealth;
+		int selfAttackDamage = this.gameObject.GetComponent<AttackingBehavior> ().attackDamage;
+		float selfAttackFrequency = this.gameObject.GetComponent<AttackingBehavior> ().attackFrequency;
+		int enemyAttackDamage = 1;
+		if (enemy.GetComponent<OnHitWithEnemy> ())
+			enemyAttackDamage = enemy.GetComponent<OnHitWithEnemy> ().damageAmount;
+		else if (enemy.GetComponent<AttackingBehavior> ())
+			enemyAttackDamage = enemy.GetComponent<AttackingBehavior> ().attackDamage;
+
+		// TODO: Figure out the equation
+		return (true);
+	}
+
+}
