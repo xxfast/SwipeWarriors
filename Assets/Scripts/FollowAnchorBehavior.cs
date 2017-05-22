a9e2525609e5399a477056455b1a6722fa6a60cb
xxfast <isuru@xfastgames.com>
2017-05-22 21:19:29 +1000

    Added the FollowAnchor behaviour and updated FollowTarget to wrk with this

---
+++ Assets/Scripts/FollowAnchorBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(FollowTarget))]
+public class FollowAnchorBehavior : MonoBehaviour {
+
+	public bool shouldMove = true;
+	public float trailDistance = 5.0f;
+	public GameObject anchorPrefab;
+
+	private GameObject anchor;
+
+	void Start () {
+		if (anchorPrefab) anchor = Instantiate (anchorPrefab);
+	}
+	
+	void Update () {
+		if (shouldMove && (anchorPrefab != null)) {
+			if (Input.GetMouseButton(0)) {
+				anchor.transform.position = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				anchor.transform.position = new Vector3 (anchor.transform.position.x, anchor.transform.position.y, 0);
+				this.gameObject.GetComponent<FollowTarget> ().target = anchor;
+			}
+		}
+	}
+}
--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class FollowTarget : MonoBehaviour {
 
 	public GameObject target;
-	[Range(0f,100f)] public float followSpeed;
-	[Range(1f,10f)] public float rotationSpeed;
+	public float followSpeed;
+	public float rotationSpeed;
+	public float minimumFollowDistance;
 
 	void Start () {
 		ApplyForceTowardTarget ();
+		if (minimumFollowDistance!=null)
+			minimumFollowDistance = target.transform.localScale.x / 2;
 	}
 
 	void Update () {
@@ -22,7 +25,7 @@ public class FollowTarget : MonoBehaviour {
 	void ApplyForceTowardTarget(){
 		if(target){
 			var d = Vector2.Distance (this.transform.position,target.transform.position);
-			if (d > target.transform.localScale.x/2){
+			if (d > minimumFollowDistance){
 				var dx = (target.transform.position.x - this.transform.position.x) / d;
 				var dy = (target.transform.position.y - this.transform.position.y) / d;
 				this.gameObject.GetComponent<Rigidbody2D> ().AddForce (new Vector2(dx * followSpeed, dy * followSpeed));
