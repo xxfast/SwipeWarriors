5a5c9009ced7f0f58fe8b82511e0810775aa6a64
xxfast <isuru@xfastgames.com>
2017-08-01 13:45:31 +1000

    implementation of a grid system on works

---
+++ Assets/Scripts/MoveOnSwipe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveOnSwipe : MonoBehaviour {
+	
+	public float speed = 100;
+	private Vector2 originalScale;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<Rigidbody2D> ().freezeRotation = true;
+		originalScale = this.gameObject.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.touchCount > 0 && Input.GetTouch(0).phase==TouchPhase.Moved){
+			Vector2 touchDirection = Input.GetTouch (0).deltaPosition.normalized;
+			Vector2 scale = originalScale;
+			if (touchDirection.x < 0)
+				scale.x *= -1;
+			this.transform.localScale = scale;
+			touchDirection.Scale (new Vector2 (speed, speed));
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (touchDirection);
+		}
+	}
+}
