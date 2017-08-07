a2dd61424717391cbd2299cef408989cd9587486
xxfast <isuru@xfastgames.com>
2017-08-07 12:50:31 +1000

    gird implementation half done

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,6 +6,8 @@ public class Health : MonoBehaviour {
 
 	public int maxHealth = 10;
 	public int currentHealth;
+	public bool showHealth = true;
+	public float healthBarLength = 1;
 
 	public bool isDead { get { return (currentHealth <= 0); } }
 
@@ -23,5 +25,19 @@ public class Health : MonoBehaviour {
 
 	public void DealDamage(int amount){
 		currentHealth -= amount;
+	}
+
+	void OnRenderObject() {
+		if(showHealth){
+			DrawQuad (new Rect(gameObject.transform.position,new Vector2(1,1)), Color.green);
+		}
+	}
+
+	void DrawQuad(Rect position, Color color) {
+		Texture2D texture = new Texture2D(1, 1);
+		texture.SetPixel(0,0,color);
+		texture.Apply();
+		GUI.skin.box.normal.background = texture;
+		GUI.Box(position, GUIContent.none);
 	}
 }
